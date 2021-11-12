fee721eaa0c515e9da39bb3ecf3efffd30bfa1d7
Sunny Bedi <sunnybedi002@yahoo.com>
2021-11-12 07:57:31 -0800

    readme questions + explanation done

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+// Nested functions can access their scope and the variable is global
 
 
 
@@ -28,10 +28,12 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num){
+  let sum = 0
+  for ( let i = 0; i < num ; i++){
+    return sum+= num
   }
+}
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
